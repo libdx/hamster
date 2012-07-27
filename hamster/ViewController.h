2c2acd0160b74a1e4c4daf9ba472ea2b8e785bf9
dx <avi.cpp@gmail.com>
2012-07-27 12:33:17 +0300

    Initial Commit

---
+++ hamster/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hamster
+//
+//  Created by Alexander Ignatenko on 7/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
