5f71c05f6b9225880830346f3c2fa1f3e239b1ac
dx <avi.cpp@gmail.com>
2012-07-27 12:43:01 +0300

    Manual initial commit

--- sources/AppDelegate.h
+++ sources/AppDelegate.h
@@ -6,14 +6,12 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-@class ViewController;
+@class RootViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) RootViewController *viewController;
 
 @end
